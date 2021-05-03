54baa1d19c96f2865964642562ca438e08145eea
tianqikai <953811508@qq.com>
2021-05-03 20:24:40 +0800

    add

--- src/javabase/wrapper/WrapperTest.java
+++ src/javabase/wrapper/WrapperTest.java
@@ -10,7 +10,7 @@ public class WrapperTest {
         System.out.println(i == j);//比较的内存地址 false
         Integer m = 1;
         Integer n = 1;
-        System.out.println(m == n);// 自动拆箱 比较的值得大小
+        System.out.println(m == n);// 下面程序不会在堆中创建对象，会直接从整型常量池中拿。
         Integer x = 128;
         Integer y = 128;
         System.out.println(x == y);// 大于127 无法自动拆箱，比较的还是内存地址
